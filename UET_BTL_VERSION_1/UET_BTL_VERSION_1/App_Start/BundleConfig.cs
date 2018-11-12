dc72253e928b680b2db0d76aac42731823ddcb02
= Xuan <=tienxuantt@gmail.com>
2018-11-12 18:32:44 +0700

    submit

--- UET_BTL_VERSION_1/UET_BTL_VERSION_1/App_Start/BundleConfig.cs
+++ UET_BTL_VERSION_1/UET_BTL_VERSION_1/App_Start/BundleConfig.cs
@@ -5,9 +5,27 @@ namespace UET_BTL_VERSION_1
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new StyleBundle("~/header/css")
+                .Include(
+                       "~/Contents/CSS/bootstrap.min.css",
+                       new CssRewriteUrlTransform()
+                        )
+               .Include(
+                       "~/Contents/CSS/dashboard.css"
+                        ));
+                
+            bundles.Add(new ScriptBundle("~/footer/js").Include(
+                      "~/Contents/js/jquery-3.3.1.min.js",
+                      "~/Contents/js/bootstrap.min.js",
+                      "~/Contents/js/admin.js"
+                      ));
+
+            bundles.Add(new StyleBundle("~/login/css").Include(
+                     "~/Contents/CSS/Login.css"
+                     ));
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
