75d4b0e3af26c03a11f73f114e09d93151585aac
= Xuan <=tienxuantt@gmail.com>
2018-11-20 09:25:44 +0700

    create delete subject

--- UET_BTL_VERSION_1/UET_BTL_VERSION_1/Contents/js/subject.js
+++ UET_BTL_VERSION_1/UET_BTL_VERSION_1/Contents/js/subject.js
@@ -13,30 +13,18 @@
             url: urldetail,
             data: { id: idCurrent },
             success: function (subject) {
-
-                var content = "";
-                var name = " </br> <span>Tên học phần :</span>";
-                var subjectCode = " </br> <span>Mã học phần :</span>";
-                var creditNumber = "</br> <span>Số tín chỉ :</span>";
-                var room = "</br> <span>Phòng học :</span>";
-                var time = "</br> <span>Thời gian,tiết :</span>";
-
-                content += name + subject.Name;
-                content += subjectCode + subject.SubjectCode;
-                content += creditNumber + subject.CreditNumber;
-                content += room + subject.ClassRoom;
-                content += time + subject.TimeTeach;
-                $(".confirm-delete-subject").html(content);
-                $(".confirm-delete-subject").append("</br></br><button class='delete'>Delete</button>");
-                $(".confirm-delete-subject").append("<button class='close1'>Close</button>");
-                $(".confirm-delete-subject").show();
-
+                $("#DsubjectName").val(subject.Name);
+                $("#DsubjectCode").val(subject.SubjectCode);
+                $("#DsubjectCredit").val(subject.CreditNumber);
+                $("#DsubjectRoom").val(subject.ClassRoom);
+                $("#DsubjectTime").val(subject.TimeTeach);
+                $(".delete-subject-form").show();
             }
         });
     });
 
-    $(".confirm-delete-subject").on("click", "button.delete", function () {
-        $(".confirm-delete-subject").slideUp(700);
+    $(".bottom-button").on("click", ".btn-delete", function () {
+        $(".delete-subject-form").fadeOut(700);
         rowCurrent.hide(1000);
         urldetail = '/Subjects/Delete';
         $.ajax({
@@ -48,8 +36,8 @@
             }
         });
     });
-    $(".confirm-delete-subject").on("click", "button.close1", function () {
-        $(".confirm-delete-subject").slideUp(700);
+    $(".bottom-button").on("click", ".btn-cancle", function () {
+        $(".delete-subject-form").fadeOut(700);
     });
     //end  ajax delete subject
 
