0338cabb28a779307a446c4b36192122cdcdb32d
= Xuan <=tienxuantt@gmail.com>
2018-11-24 07:39:45 +0700

    add comment code

--- UET_BTL_VERSION_1/UET_BTL_VERSION_1/Areas/Admin/Controllers/HomeController.cs
+++ UET_BTL_VERSION_1/UET_BTL_VERSION_1/Areas/Admin/Controllers/HomeController.cs
@@ -13,18 +13,24 @@ namespace UET_BTL_VERSION_1.Areas.Admin.Controllers
     [AuthorizeBusiness]
     public class HomeController : Controller
     {
+        // Khởi tạo đối tượng DBcontext để thao tác với CSDL
         private UetSurveyDbContext db = new UetSurveyDbContext();
 
+        // Hiển thị trang chủ của index gồm số người online, số sinh viên, giảng viên ...
         public ActionResult Index()
         {
+            // Kiểm tra session đã tồn tại chưa 
             if (Session["user"] != null)
-            {
+            { 
+                // Gán các tham số tống số sinh viên, môn học, đánh giá, người online để truyền sang view
                 ViewBag.SumStudents = db.Students.ToList().Count();
                 ViewBag.SumSubjects = db.Subjects.ToList().Count();
                 ViewBag.SumSurveys = db.Surveys.GroupBy(x => x.StudentDetailID).Count();
                 ViewBag.SumUserOnline = HttpContext.Application["Online"].ToString();
+
                 return View();
             }
+            // Chuyển hướng đến trang login
             return RedirectToAction("Login", "Home",new { area = "SignIn"});
         }
        
--- UET_BTL_VERSION_1/UET_BTL_VERSION_1/Areas/Admin/Controllers/StudentManagerController.cs
+++ UET_BTL_VERSION_1/UET_BTL_VERSION_1/Areas/Admin/Controllers/StudentManagerController.cs
@@ -15,39 +15,52 @@ namespace UET_BTL_VERSION_1.Areas.Admin.Controllers
     [AuthorizeBusiness]
     public class StudentManagerController : Controller
     {
+        // Khởi tạo đối tượng DBcontext để thao tác với CSDL
         private UetSurveyDbContext db = new UetSurveyDbContext();
 
+        // Hiển thị danh sách các sinh viên theo phân trang
         [HttpGet]
         public ActionResult Index(int? page)
         {
+                // Số sinh viên trên một trang
                 int pageSize   = 20;
+                // Số trang hiện tại
                 int pageNumber = (page ?? 1);
+                // Tổng số sinh viên
                 ViewBag.sum    = db.Students.Count();
                 return View(db.Students.ToList().ToPagedList(pageNumber, pageSize));
         }
 
+        // Trả về kết quả khi tìm kiếm 
         [HttpPost]
         public ActionResult Index(FormCollection f, int? page)
         {
+            // Lấy từ khóa tìm kiếm từ thanh search
             string sTuKhoa = f["txtTimKiem"].ToString();
+            // Truyền từ khóa tìm kiếm sang view hiển thị
             ViewBag.tukhoa = sTuKhoa;
+            // Lấy danh sách các học sinh có chứa từ khóa tìm kiếm
             List<Student> listKQ = db.Students.Where(x => x.Name.Contains(sTuKhoa)).ToList();
             int pageSize   = 200;
             int pageNumber = (page ?? 1);
+            // Kiểm tra xem danh sách tìm kiếm có rỗng không
             if (listKQ.Count == 0)
             {
                 ViewBag.ThongBao = "Không tìm thấy sinh viên nào";
                 return View(db.Students.ToList().ToPagedList(pageNumber, pageSize));
             }
+            // Truyền thông báo ra view
             ViewBag.messageSearch = "Kết quả tìm kiếm với từ khóa : " + sTuKhoa;
             ViewBag.sum = listKQ.Count();
             return View(listKQ.ToPagedList(pageNumber, pageSize));
         }
 
+        // Thêm mới sinh viên và trả về json cho client
         [HttpPost]
-        public ActionResult Create(FormCollection form)
+        public JsonResult Create(FormCollection form)
         {
             db.Configuration.ProxyCreationEnabled = false;
+            // Lấy username và kiếm tra xem đã tồn tại chưa
             string username = form["UserName"].ToString();
             if (db.Users.Any(x => x.UserName.Equals(username)))
             {
@@ -55,6 +68,7 @@ namespace UET_BTL_VERSION_1.Areas.Admin.Controllers
             }
             else
             {
+                // Khởi tạo sinh viên, gán giá trị và lưu vào DB
                 Student stu     = new Student();
                 stu.Name        = form["Name"].ToString();
                 stu.Course      = form["Course"].ToString();
@@ -65,6 +79,7 @@ namespace UET_BTL_VERSION_1.Areas.Admin.Controllers
                 db.Students.Add(stu);
                 db.SaveChanges();
                 int studentid   = db.Students.Max(x => x.StudentID);
+                // Khởi tạo user, gán trị sinh viên vừa lưu vào và lưu vào DB
                 User user = new User()
                 {
                     UserName    = stu.UserName,
@@ -78,18 +93,23 @@ namespace UET_BTL_VERSION_1.Areas.Admin.Controllers
             }
         }
 
+        // Chỉnh sửa sinh viên
         [HttpPost]
         public ActionResult Edit(FormCollection form)
         {
             db.Configuration.ProxyCreationEnabled = false;
+            // Lấy username từ form submit lên
             string username = form["UserName"].ToString();
+            // Lấy id của sinh viên từ form
             int idStudent = int.Parse(form["idStudent"].ToString());
+            // Kiểm tra xem sinh viên có bị trùng lặp không
             if (db.Users.Any(x => x.UserName.Equals(username) && x.StudentID != idStudent))
             {
                 return Json(new { status = 0 }, JsonRequestBehavior.AllowGet);
             }
             else
             {
+                // Cập nhật thông tin cho user và sinh viên theo các giá trị được submit từ form
                 User user       = db.Users.FirstOrDefault(x => x.StudentID == idStudent);
                 user.UserName   = username;
                 user.PassWord   = form["PassWord"].ToString();
@@ -105,6 +125,7 @@ namespace UET_BTL_VERSION_1.Areas.Admin.Controllers
             }
         }
 
+        // Xóa sinh viên theo id
         [HttpGet]
         public ActionResult Delete(int? id)
         {
@@ -121,17 +142,23 @@ namespace UET_BTL_VERSION_1.Areas.Admin.Controllers
             return Json(student, JsonRequestBehavior.AllowGet);
         }
 
+        // Xác nhận xóa
         [HttpPost]
         public ActionResult Delete(int id)
         {
+            // Tìm kiếm sinh viên theo mã
             Student student = db.Students.FirstOrDefault(x => x.StudentID == id);
+            // Tìm kiếm user theo mã sinh viên
             User user       = db.Users.FirstOrDefault(x => x.StudentID == id);
+            // Lấy ra danh sách sinh viên chi tiết theo mã
             IEnumerable<StudentDetail> list2 = db.StudentDetails.Where(x => x.StudentID == id);
             foreach (var item in list2)
             {
+                // Lấy ra các survey của sinh viên đó
                 IEnumerable<Survey> list3 = db.Surveys.Where(x => x.StudentDetailID == item.StudentDetailID);
                 try
                 {
+                    // Xóa toàn bộ survey của sinh viên đó
                     db.Surveys.RemoveRange(list3);
                 }
                 catch (Exception)
@@ -139,41 +166,53 @@ namespace UET_BTL_VERSION_1.Areas.Admin.Controllers
 
                 }
             }
+            // Xóa toàn bộ sinh viên trong các lớp học phần
             db.StudentDetails.RemoveRange(list2);
+            // Xóa user đại diện cho sinh viên đó
             db.Users.Remove(user);
+            // Xóa sinh viên đó
             db.Students.Remove(student);
             db.SaveChanges();
             return Json(new { status = 1 }, JsonRequestBehavior.AllowGet);
         }
 
+        // Impoert sinh viên từ excel từ fileUpload
         [HttpPost]
         public ActionResult ImportStudent(HttpPostedFileBase fileUpload)
         {
             ViewBag.message = "Import không thành công";
             int count = 0;
+            // Lấy file đã chọn để thao tác 
             var package = new ExcelPackage(fileUpload.InputStream);
             if (ImportData(out count, package))
             {
                 ViewBag.message = "Import thành công";
             }
+            // Thông báo tổng số sinh viên được import
             ViewBag.countStudent = count;
             return View();
         }
 
+        // Import data 
         public bool ImportData(out int count, ExcelPackage package)
         {
             count = 0;
             var result = false;
             try
             {
+                // Bắt đầu từ cột 1
                 int startColumn = 1;
+                // Bắt đầu từ hàng 2
                 int startRow = 2;
+                // Lấy sheet 1 của file excel
                 ExcelWorksheet workSheet = package.Workbook.Worksheets[1];
                 object data = null;
+                // Khởi tạo đối tượng context để thao tác csdl
                 UetSurveyDbContext db = new UetSurveyDbContext();
 
                 do
                 {
+                    // Lấy các giá trị của từng hàng 
                     data = workSheet.Cells[startRow, startColumn].Value;
                     object userName = workSheet.Cells[startRow, startColumn + 1].Value;
                     object passWord = workSheet.Cells[startRow, startColumn + 2].Value;
@@ -181,8 +220,10 @@ namespace UET_BTL_VERSION_1.Areas.Admin.Controllers
                     object email = workSheet.Cells[startRow, startColumn + 4].Value;
                     object course = workSheet.Cells[startRow, startColumn + 5].Value;
 
+                    // Kiểm tra xem đã cuối danh sách excel chưa
                     if (data != null)
                     {
+                        // Truyền dữ liệu một sinh viên vào lưu vào DB
                         var isSuccess = SaveStudent(userName.ToString(), passWord.ToString(), fullName.ToString(), email.ToString(), course.ToString(), db);
                         if (isSuccess)
                         {
@@ -201,13 +242,16 @@ namespace UET_BTL_VERSION_1.Areas.Admin.Controllers
             return result;
         }
 
+        // Lưu mỗi sinh viên vào DB
         public bool SaveStudent(string userName, string passWord, string fullName, string email, string course, UetSurveyDbContext db)
         {
             var result = false;
             try
             {
-                if (db.Students.Where(x => x.UserName.Equals(userName)).Count() == 0)
+                // Kiểm tra xem sinh viên đã tồn tại chưa
+                if (db.Students.Any(x => x.UserName.Equals(userName)))
                 {
+                    // Khởi tạo sinh viên và gán dữ liệu rồi lưu vào db
                     var stu         = new Student();
                     stu.UserName    = userName;
                     stu.PassWord    = passWord;
@@ -217,7 +261,9 @@ namespace UET_BTL_VERSION_1.Areas.Admin.Controllers
                     stu.Course      = course;
                     db.Students.Add(stu);
                     db.SaveChanges();
+                    // Lấy Id sinh viên vừa tạo 
                     int studentid   = db.Students.Max(x => x.StudentID);
+                    // Khởi tạo user rồi gán giá trị sinh viên vừa lưu
                     User user = new User()
                     {
                         UserName    = userName,
